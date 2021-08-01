1b2d29c048d360ab01b0a4cd41ced95bebd70f8f
kyle <kyleholzinger@hey.com>
2021-07-31 20:57:46 -0400

    [KH] add ant design

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import { Timeline } from 'antd'
+
+import s from './App.module.css'
 
 const App = () => {
-  return <div>Hey lkasjdskj</div>
+  return (
+    <div>
+      <Timeline className={s.timelineContainer} pending="Recording...">
+        <Timeline.Item>Create a services site 2015-09-01</Timeline.Item>
+        <Timeline.Item>Solve initial network problems 2015-09-01</Timeline.Item>
+        <Timeline.Item>Technical testing 2015-09-01</Timeline.Item>
+      </Timeline>
+    </div>
+  )
 }
 
 export default App
